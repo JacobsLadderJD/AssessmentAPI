209690bc014fa71ce9ad1d0b6d40315070a2490d
Joshua <jmichael652@gmail.com>
2019-03-05 22:41:46 -0500

    Adds Evaluation to admin panel

--- jlc_api/quickstart/admin.py
+++ jlc_api/quickstart/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import Student
+from .models import Evaluation
 
 # Register your models here.
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Evaluation)
+class EvaluationAdmin(admin.ModelAdmin):
     pass
 
